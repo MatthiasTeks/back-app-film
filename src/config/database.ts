b0d960f6dfc6098b424cd20d1abdc039c4318b2b
MatthiasTeks <mathias.vimbert@gmail.com>
2023-04-04 18:18:41 +0200

    fix heroku deploy

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,6 +2,7 @@ import { dbConfig } from "./db";
 import { createPool, PoolConnection } from "mysql2/promise";
 
 async function createDBConnection(): Promise<PoolConnection> {
+    console.log(dbConfig)
     const pool = createPool(dbConfig);
     return pool.getConnection();
 }
