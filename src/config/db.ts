118361f0412bd24d92f5e06db38fd7316f866296
MatthiasTeks <mathias.vimbert@gmail.com>
2023-04-04 18:16:19 +0200

    fix heroku deploy

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const dbConfig = {
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || "3306", 10),
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
--- src/config/index.ts
+++ src/config/index.ts
@@ -6,5 +6,5 @@ export const config: { aws_access_key_id: string, aws_secret_access_key:string, 
     aws_access_key_id : process.env.AWS_ACCESS_KEY_ID ?? " ",
     aws_secret_access_key : process.env.AWS_SECRET_ACCESS_KEY ?? '',
     aws_region : process.env.AWS_REGION ?? '',
-    bucket_name: process.env.BUCKET_NAME ?? 'test-bucket'
+    bucket_name: process.env.AWS_S3_BUCKET_NAME ?? 'test-bucket'
 }
